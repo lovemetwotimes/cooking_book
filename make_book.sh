377c09c98edee2591771debb5bdf99f833800b78
lovemetwotimes <viperboy@poczta.fm>
2022-04-11 13:44:01 +0200

    fix newpages in pdf export

--- make_book.sh
+++ make_book.sh
@@ -1,4 +1,12 @@
-chapters=$(find . -name "*.markdown")
+chapters=$(find . -name "*.markdown" | sort)
 
 pandoc --toc --number-section  -o przepisy.epub title.txt $chapters
+gawk '1; ENDFILE{print "\\newpage"}' $chapters | pandoc --standalone \
+    -f markdown+smart \
+    --self-contained \
+    --toc \
+    --number-section  \
+    -o przepisy.pdf  \
+    --pdf-engine=xelatex 
+
 ebook-convert przepisy.epub przepisy.mobi
